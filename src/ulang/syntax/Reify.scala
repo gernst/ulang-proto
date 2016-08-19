658347286e491c0d57c3b6639cacdcd680a216b9
Gidon Ernst <ernst@isse.de>
2016-08-19 15:19:54 +0200

    fix reification

--- src/ulang/syntax/Reify.scala
+++ src/ulang/syntax/Reify.scala
@@ -14,18 +14,23 @@ object Reify {
     exprs.foldRight(apply("Nil"))(apply("Cons", _, _))
   }
 
+  def reify(cs: Case): Expr = cs match {
+    case Case(bound, body) =>
+      apply("Case", reify(bound), reify(body))
+  }
+
   def reify(expr: Expr): Expr = expr match {
     case name: Id =>
       apply("Id", name)
+
+    case Constr(name, args) =>
+      apply("Constr", apply(name), list(args))
 
     case Apply(fun, arg) =>
       apply("Apply", reify(fun), reify(arg))
 
     case Match(cases) =>
-      val args = cases map {
-        case Case(bound, body) =>
-          apply("Case", reify(bound), reify(body))
-      }
+      val args = cases map reify
       apply("Match", list(args))
   }
 }
